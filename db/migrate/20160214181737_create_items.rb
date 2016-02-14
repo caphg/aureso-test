f25ccb50a6e03674036aa734e33aec3dbcdfb7dd
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-02-14 19:18:13 +0100

    create item model

---
+++ db/migrate/20160214181737_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :qnt
+      t.integer :value
+      t.string :category
+      t.string :subcategory
+      t.text :tag, array: true, default: []
+
+      t.timestamps null: false
+    end
+  end
+end
