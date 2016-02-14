485c97009ec0b7ece6788b093c82c49d97f163df
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-02-14 19:09:31 +0100

    change order primary key

--- db/migrate/20160214180216_create_orders.rb
+++ db/migrate/20160214180216_create_orders.rb
@@ -1,6 +1,6 @@
 class CreateOrders < ActiveRecord::Migration
   def change
-    create_table :orders do |t|
+    create_table(:orders, primary_key: 'order_id') do |t|
       t.string :email
       t.decimal :total_amount_net
       t.decimal :shipping_costs
