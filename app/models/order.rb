9db1ae36956fab12def81ca3f448d4e49344aaa4
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-02-14 19:27:06 +0100

    add order spec, order model helpers

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ActiveRecord::Base
 	has_many :items, dependent: :destroy
 	accepts_nested_attributes_for :items, allow_destroy: true
 
+	COLLECTIONS = [7,12]
+
+	def is_discount_value_positive?
+		(self.items.map{|item| item.collection_id}.uniq - COLLECTIONS.uniq).length == 0
+	end
+
+	def order_value
+		total_amount_net.to_f - shipping_costs.to_f
+	end
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe "/orders", type: :request do
+  describe "POST" do
+    it "has discount 0 beacuse item not in collection" do
+      post "/api/v1/orders.json", {
+      	"parameters": {
+	      	"order": {
+		      	"order_id": 12345,
+		      	"email": "test@test.com",
+		      	"total_amount_net": "39.00",
+		      	"shipping_costs": "29.00",
+		      	"payment_method": "VISA",
+		      	"items": [{
+		      	"name": "item1",
+		      	"qnt": 1,
+		      	"value": 1100,
+		      	"category": "fashion",
+		      	"subcategory": "jacket",
+		      	"tags": ["tag1", "tag2"],
+		      	"collection_id": 42
+		      	}]
+	      	}
+      	}
+      }
 
+      expect(json).to eq(
+      	{
+      	    "id" => 12345,
+      	    "email" => "test@test.com",
+      	    "total_amount_net" => "39.0",
+      	    "shipping_costs" => "29.0",
+      	    "payment_method" => "VISA",
+      	    "discount_value" => 0,
+      	    "items" => [
+      	        {
+      	            "name" => "item1",
+      	            "qnt" => 1,
+      	            "value" => 1100,
+      	            "category" => "fashion",
+      	            "subcategory" => "jacket",
+      	            "collection_id" => 42,
+      	            "tags" => [
+      	                "tag1",
+      	                "tag2"
+      	            ]
+      	        }
+      	    ]
+      	}
+      )
+      expect(response.status).to eq(201)
+    end
+  end
 end
